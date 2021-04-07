e101d74aae27e63b63b137b01681ce7624a744d4
WaspGeekBrains <wasporc@mail.ru>
2021-04-08 00:32:28 +0300

    security update

--- src/main/java/ru/hiber/cofig/SecurityConfig.java
+++ src/main/java/ru/hiber/cofig/SecurityConfig.java
@@ -18,30 +18,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.userService = userService;
     }
 
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//    }
-//    @Bean
-//    public DaoAuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
-//        auth.setUserDetailsService(userService);
-//        auth.setPasswordEncoder(passwordEncoder());
-//        return auth;
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.addFilterAfter(tokenAuthenticationFilter(), BearerTokenAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/**").hasAnyRole("USER", "ADMIN")
-                //.antMatchers("/app/products").hasRole("ADMIN")
+                .antMatchers("/").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/persons/*").hasRole("ADMIN")
                 .and()
                 .formLogin()
                 .loginPage("/login")
